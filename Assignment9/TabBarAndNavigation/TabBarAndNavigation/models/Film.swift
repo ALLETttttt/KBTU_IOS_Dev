1f223ae401f7bc75e7ef4c0f6834ad2dac53da8c
bakdaulet <bakdauletcoutinho@gmail.com>
2024-11-15 08:30:52 +0500

    commit

---
+++ Assignment9/TabBarAndNavigation/TabBarAndNavigation/models/Film.swift
@@ -0,0 +1,16 @@
+//
+//  Film.swift
+//  TabBarAndNavigation
+//
+//  Created by Бакдаулет on 15.11.2024.
+//
+
+import UIKit
+
+struct Film {
+    let title: String
+    let imageName: UIImage
+    let genre: String
+    let description: String
+    let popularity: String
+}
---
+++ Assignment9/TabBarAndNavigation/TabBarAndNavigation/models/Music.swift
@@ -0,0 +1,16 @@
+//
+//  Music.swift
+//  TabBarAndNavigation
+//
+//  Created by Бакдаулет on 15.11.2024.
+//
+
+import UIKit
+
+struct Music {
+    let title: String
+    let imageName: UIImage
+    let artist: String
+    let album: String
+    let description: String
+}
